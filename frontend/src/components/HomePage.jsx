0189793db55e29b340bbcac689d187d47d7af878
vikash-pandey1 <vikashpandey7082@gmail.com>
2024-07-10 17:49:10 +0530

    add file in components

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import Sidebar from './Sidebaar'
+import React, { useEffect } from 'react'
+import Sidebar from './Sidebar'
 import MessageContainer from './MessageContainer'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
-function HomePage() {
+const HomePage = () => {
+  const { authUser } = useSelector(store => store.user);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!authUser) {
+      navigate("/login");
+    }
+  }, []);
   return (
-    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden  bg-clip-padding backdrop-filter backdrop-blur-lg ' >
-      <Sidebar></Sidebar>
-      <MessageContainer/>
+    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+      <Sidebar />
+      <MessageContainer />
     </div>
   )
 }
