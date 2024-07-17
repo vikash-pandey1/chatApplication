f43187b56c0a09709c9eef2f338a32da1d17e92b
vikash-pandey1 <vikashpandey7082@gmail.com>
2024-07-17 18:26:36 +0530

    complete chating project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,68 +1,59 @@
+import Signup from "./components/Signup";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import Login from "./components/Login";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import io from "socket.io-client";
+import { setSocket } from "./redux/socketSlice";
+import { setOnlineUsers } from "./redux/userSlice";
 
-import {createBrowserRouter,RouterProvider} from "react-router-dom"
-import Signup from "./components/Signup"
-import Login from './components/Login'
-import HomePage from './components/HomePage'
-import { useDispatch, useSelector } from "react-redux"
-import { useEffect, useState } from "react"
-import {io} from 'socket.io-client'
-import { setSocket } from "./redux/socketSlice"
-import { setOnlineUsers } from "./redux/userSlice"
 const router = createBrowserRouter([
   {
-    path:"/",
-    element:<HomePage/>
+    path: "/",
+    element: <HomePage />,
   },
   {
-    path:"/signup",
-    element:<Signup/>
+    path: "/signup",
+    element: <Signup />,
   },
   {
-    path:"/login",
-    element:<Login/>
-  }
-])
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 function App() {
-  const {authUser} = useSelector(store=>store.user)
-  const {socket} = useSelector(store=>store.socket)
+  const { authUser } = useSelector((store) => store.user);
+  const { socket } = useSelector((store) => store.socket);
+  const dispatch = useDispatch();
 
-
-  const dispatch = useDispatch()
-
-  useEffect(()=>{
-    if(authUser){
-      const socket = io('http://localhost:8081',{
-        withCredentials:true,
-        extraHeaders:{
-          "my-custom-header":"abcd"
+  useEffect(() => {
+    if (authUser) {
+      const socketio = io("http://localhost:8081", {
+        query: {
+          userId: authUser._id,
         },
-        query:{
-          userId:authUser._id
-        }
       });
+      dispatch(setSocket(socketio));
 
-      dispatch(setSocket(socket));
-      socket.on('getOnlineUsers',(onlineUsers)=>{
-        dispatch(setOnlineUsers(onlineUsers))
+      socketio?.on("getOnlineUsers", (onlineUsers) => {
+        dispatch(setOnlineUsers(onlineUsers));
       });
-      return ()=> socket.close();
-    }else{
-      if(socket){
+      return () => socketio.close();
+    } else {
+      if (socket) {
         socket.close();
         dispatch(setSocket(null));
       }
     }
-
-  },[authUser]);
+  }, [authUser]);
 
   return (
-    <>
     <div className="p-4 h-screen flex items-center justify-center">
       <RouterProvider router={router} />
     </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {Toaster} from 'react-hot-toast'
-import {Provider} from "react-redux"
-import store from './redux/store.js'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+let persistor = persistStore(store);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
-    <Toaster/>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+        <Toaster />
+      </PersistGate>
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
