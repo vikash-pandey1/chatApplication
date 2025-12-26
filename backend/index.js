6daeefce178c0881bc2414b6b696ccc3f4e3a82c
vikash-pandey1 <vikashpandey7082@gmail.com>
2025-12-26 15:17:04 +0530

    now change

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ import userRoute from "./routes/userRoute.js";
 import messageRoute from "./routes/messageRoute.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { app, server } from "./socket/socket.js"
+import { app, server } from "./Socket/Socket.js"
 import path from "path";
 
 
